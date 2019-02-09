6e41abdb3ebeaeb73fe05b01970f1a9e86a2cd20
asef <asef@Lenovo-PC.localdomain>
2019-02-09 00:36:36 -0500

    initial version on Unique hw2 complete

--- hw2/hw2/Unique.java
+++ hw2/hw2/Unique.java
@@ -1,9 +1,14 @@
 /* TODO - Add your name, JHED, and email.
+    Asef Islam
+    aislam5
+    aislam5@jhu.edu
  * Unique.java
  */
 
 package hw2;
 
+import java.util.Iterator;
+import java.util.Scanner;
 
 /** Unique problem using a SparseArray and processing from standard in. */
 public final class Unique {
@@ -16,6 +21,36 @@ public final class Unique {
      * @param args Command line arguments.
      */
     public static void main(String[] args) {
-        // TODO
+        Scanner scan = new Scanner(System.in);
+        SimpleArray<Integer> nums = new SimpleArray<Integer>(10, 0);
+        int lastind = 0;
+        while(scan.hasNext()) {
+            try {
+                int thisnum = Integer.parseInt(scan.next());
+                int isunique = 0;
+                for (int q=0; q<lastind;q++) {
+                    if(thisnum ==nums.get(q)) {
+                        isunique = 1;
+                    }
+                }
+                if (isunique == 0)   { //if it is unique
+                    if(lastind + 2 > nums.length()) { //let's double the size
+                        SimpleArray<Integer> temp = nums;
+                        nums = new SimpleArray<Integer>(temp.length() * 2, 0);
+                        for(int l = 0; l < temp.length(); l++) {
+                            nums.put(l, temp.get(l));
+                        }
+                    }
+                    nums.put(lastind,thisnum);
+                    lastind = lastind + 1;
+                }
+            }
+            catch (NumberFormatException e) {
+                throw new IllegalArgumentException("arguments must be integers");
+            }
+        }
+        for(int i=0; i<lastind;i++) { //let's print
+            System.out.println(nums.get(i));
+        }
     }
 }
