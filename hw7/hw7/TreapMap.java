3f1e32afd195593a7b3abc51c816691c9ceed043
asef <asef@Lenovo-PC.localdomain>
2019-04-07 15:57:19 -0400

    seemed to fix treap

--- hw7/hw7/TreapMap.java
+++ hw7/hw7/TreapMap.java
@@ -25,6 +25,15 @@ public class TreapMap<K extends Comparable<? super K>, V>
             this.height = 0;
         }
 
+        // Constructor with priority argument,
+        Node(K k, V v, int p) {
+            // left and right default to null
+            this.key = k;
+            this.value = v;
+            this.priority = p;
+            this.height = 0;
+        }
+
         // Just for debugging purposes.
         public String toString() {
             return "Node<key: " + this.key
@@ -36,12 +45,14 @@ public class TreapMap<K extends Comparable<? super K>, V>
         public int balance() {
             return height(this.left) - height(this.right);
         }
+
     }
 
     private Node root;
     private int size;
     private StringBuilder stringBuilder;
     Random rand = new Random();
+    protected ArrayList<Node> nodeList;
 
     @Override
     public int size() {
@@ -56,6 +67,33 @@ public class TreapMap<K extends Comparable<? super K>, V>
         return height(this.root);
     }
 
+    public Node getRoot() {return this.root;};
+
+    public Node getLeft(Node n) {
+        return n.left;
+    }
+
+    public Node getRight(Node n) {
+        return n.right;
+    }
+
+    public V getVal(Node n) {
+        return n.value;
+    }
+
+    public int getPriority(Node n) {
+        return n.priority;
+    }
+
+    public ArrayList<Node> inorder(Node root, ArrayList<Node> list) {
+        if (root == null) {
+            return list;
+        }
+        inorder(root.left,list);
+        list.add(root);
+        inorder(root.right,list);
+        return list;
+    }
 
     // Return node for given key. This one is iterative
     // but the recursive one from lecture would work as
@@ -162,12 +200,10 @@ public class TreapMap<K extends Comparable<? super K>, V>
 
     private Node treapify(Node n) {
         if (n.left != null
-                && (n.balance() > 1
-                || n.left.priority > n.priority)) {
+                && n.left.priority > n.priority) {
             return rotateRight(n);
         } else if (n.right != null
-                && (n.balance() < -1
-                || n.right.priority > n.priority)) {
+                && n.right.priority > n.priority) {
             return rotateLeft(n);
         }
         return n;
@@ -192,12 +228,39 @@ public class TreapMap<K extends Comparable<? super K>, V>
         return treapify(n);
     }
 
+    private Node insert(Node n, K k, V v, int p) {
+        if (n == null) {
+            return new Node(k, v, p);
+        }
+
+        int cmp = k.compareTo(n.key);
+        if (cmp < 0) {
+            n.left = this.insert(n.left, k, v, p);
+
+        } else if (cmp > 0) {
+            n.right = this.insert(n.right, k, v, p);
+
+        } else {
+            throw new IllegalArgumentException("duplicate key " + k);
+        }
+        n.height = 1 + Math.max(height(n.left), height(n.right));
+        return treapify(n);
+    }
+
     @Override
     public void insert(K k, V v) throws IllegalArgumentException {
         if (k == null) {
             throw new IllegalArgumentException("null key");
         }
         this.root = this.insert(this.root, k, v);
+        this.size++;
+    }
+
+    public void insert(K k, V v, int p) throws IllegalArgumentException {
+        if (k == null) {
+            throw new IllegalArgumentException("null key");
+        }
+        this.root = this.insert(this.root, k, v, p);
         this.size++;
     }
 
@@ -221,8 +284,9 @@ public class TreapMap<K extends Comparable<? super K>, V>
     {
         this.size -= 1;
         Node n = this.findForSure(k);
+        V val = n.value;
         root = remove(k, root);
-        return n.value;
+        return val;
     }
 
     /**
--- hw7/hw7/tests/TreapMapTest.java
+++ hw7/hw7/tests/TreapMapTest.java
@@ -3,6 +3,13 @@ package hw7.tests;
 import hw7.Map;
 import hw7.OrderedMap;
 import hw7.TreapMap;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
 
 /** Instantiate the TreapMap to test. */
 public class TreapMapTest extends OrderedMapTest {
@@ -11,5 +18,82 @@ public class TreapMapTest extends OrderedMapTest {
     protected OrderedMap<Integer, String> createMap() {
         return new TreapMap<>();
     }
+
+    protected TreapMap<Integer, String> treap = new TreapMap<>();
+
+    @Test
+    public void leftRotation() {
+        treap.insert((Integer) 2, "A", 2);
+        treap.insert((Integer) 1, "B", 1);
+        treap.insert((Integer) 3, "C", 3);
+        assertEquals("{1: B, 2: A, 3: C}", treap.toString());
+        assertEquals("C", treap.getVal(treap.getRoot()));
+        assertEquals(2, treap.height());
+        assertEquals(2, treap.balance());
+        assertEquals(3, treap.size());
+    }
+
+    @Test
+    public void rightRotation() {
+        treap.insert((Integer) 2, "A", 2);
+        treap.insert((Integer) 3, "B", 1);
+        treap.insert((Integer) 1, "C", 3);
+        assertEquals("{1: C, 2: A, 3: B}", treap.toString());
+        assertEquals("C", treap.getVal(treap.getRoot()));
+        assertEquals(2, treap.height());
+        assertEquals(-2, treap.balance());
+        assertEquals(3, treap.size());
+    }
+
+    @Test
+    public void testTreap() {
+        treap.insert((Integer) 2, "B", 1);
+        treap.insert((Integer) 9, "S", 2);
+        treap.insert((Integer) 7, "Q", 3);
+        treap.insert((Integer) 8, "R", 4);
+        treap.insert((Integer) 10, "T", 0);
+        assertEquals(5, treap.size());
+        assertEquals(4, treap.getPriority(treap.getRoot()));
+        assertEquals("R", treap.getVal(treap.getRoot()));
+    }
+
+    @Test
+    public void bigTreapTest() {
+        treap.insert((Integer) 1, "A", 5);
+        treap.insert((Integer) 3, "B", 9);
+        treap.insert((Integer) 5, "C", 10);
+        treap.insert((Integer) 7, "D", 30);
+        treap.insert((Integer) 9, "E", 25);
+        treap.insert((Integer) 11, "F", 27);
+        treap.insert((Integer) 13, "G", 19);
+        treap.insert((Integer) 15, "H", 35);
+        treap.insert((Integer) 17, "I", 35);
+        treap.insert((Integer) 19, "J", 77);
+        treap.insert((Integer) 21, "K", 96);
+        treap.insert((Integer) 23, "L", 42);
+        treap.insert((Integer) 25, "M", 13);
+        treap.insert((Integer) 27, "N", 17);
+        treap.insert((Integer) 29, "O", 46);
+        treap.insert((Integer) 31, "P", 44);
+        treap.insert((Integer) 2, "Q", 86);
+        treap.insert((Integer) 6, "R", 13);
+        treap.insert((Integer) 10, "S", 31);
+        treap.insert((Integer) 14, "T", 11);
+        treap.insert((Integer) 18, "U", 41);
+        treap.insert((Integer) 22, "V", 34);
+        treap.insert((Integer) 26, "W", 22);
+        treap.insert((Integer) 30, "X", 12);
+        treap.insert((Integer) 4, "Y", 14);
+        treap.insert((Integer) 12, "Z", 34);
+        treap.insert((Integer) 20, "!", 64);
+        treap.insert((Integer) 28, "@", 45);
+        treap.insert((Integer) 8, "#", 32);
+        treap.insert((Integer) 16, "$", 11);
+        treap.insert((Integer) 24, "%", 34);
+        assertEquals(31, treap.size());
+        assertEquals(96, treap.getPriority(treap.getRoot()));
+        assertEquals("K", treap.getVal(treap.getRoot()));
+    }
+
 
 }
